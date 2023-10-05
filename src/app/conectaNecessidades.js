c1cfb19efbe7331ff6b5ce9caa17db9334fe4d70
Felipe De Andrade <felipe.deam98@gmail.com>
2023-10-05 09:17:27 -0300

    sistema de registros e consumo de API

---
+++ src/app/conectaNecessidades.js
@@ -0,0 +1,29 @@
+async function listaONGs() {
+  const conexao = await fetch(`http://localhost:3000/servicos`);
+  const conexaoConvertida = await conexao.json();
+  console.table(conexaoConvertida);
+  return conexaoConvertida;
+}
+
+async function cadastrarNecessidade(descricao, necessidades, idONG) {
+  const conexao = await fetch('http://localhost:3000/servicos', {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+      descricao: descricao,
+      necessidades: necessidades,
+      idONG: idONG,
+    })
+  });
+
+  const conexaoConvertida = await conexao.json();
+  return conexaoConvertida;
+}
+
+
+export const conectaApi = {
+  cadastrarNecessidade,
+  listaONGs,
+}
